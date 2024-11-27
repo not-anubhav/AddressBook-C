3d5aa012b8081b7859a51b818814555915d8268a
Anubhav Anand <notanubhav@gmail.com>
2024-11-27 14:33:59 +0000

    Updated: Field 1 I/O

--- AddressBook-NewDesign copy/file.c
+++ AddressBook-NewDesign copy/file.c
@@ -4,7 +4,7 @@ void saveContactsToFile(AddressBook *addressBook)
 {
     FILE* fptr = fopen("contacts.csv", "w");
 
-    fprintf(fptr, "%s,%s,%d\n", "Contact", "Count", addressBook->contactCount);
+    fprintf(fptr, "#%d NAME,PHONE,EMAIL\n", addressBook->contactCount);
     for ( int i = 0; i < addressBook->contactCount; i++)
     {
         fprintf(fptr, "%s,%s,%s\n", addressBook->contacts[i].name, addressBook->contacts[i].phone, addressBook->contacts[i].email);
@@ -22,8 +22,8 @@ int loadContactsFromFile(AddressBook *addressBook)
         printf("\n>>>>FILE DOES NOT EXIST<<<<\n");
         return 0;
     }
-    fseek(fptr, 14, SEEK_SET);
-    fscanf(fptr, "%d\n", &addressBook->contactCount);
+    //fseek(fptr, 14, SEEK_SET);
+    fscanf(fptr, "#%d NAME,PHONE,EMAIL\n", &addressBook->contactCount);
     for(int i = 0; i < addressBook->contactCount; i++)
     {
         fscanf(fptr, "%[a-z A-Z],%[0-9],%s\n", addressBook->contacts[i].name, addressBook->contacts[i].phone, addressBook->contacts[i].email);
