d04b42253bc539db0929ebd91bc67784c15a96d9
Anubhav Anand <notanubhav@gmail.com>
2024-11-28 04:05:47 +0000

    Added: Comments

--- AddressBook/file.h
+++ AddressBook/file.h
@@ -1,9 +1,9 @@
 #ifndef FILE_H
-#define FILE_H
+#define FILE_H // Define a header file with name file.h
 
-#include "contact.h"
+#include "contact.h" // // Include contact header file having AddressBook structure
 
-void saveContactsToFile(AddressBook *addressBook);
-int loadContactsFromFile(AddressBook *addressBook);
+void saveContactsToFile(AddressBook *addressBook); // Function Declaration for saveContactsToFile Function
+int loadContactsFromFile(AddressBook *addressBook); // Function Declaration for loadContactsFromFile Function
 
 #endif
