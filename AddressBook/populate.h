58896d1f1daeb0fc7c426bdf42432feb70aedb3a
Anubhav Anand <notanubhav@gmail.com>
2024-11-28 04:08:29 +0000

    Added: Comments

--- AddressBook/populate.h
+++ AddressBook/populate.h
@@ -1,2 +1,3 @@
-#include "contact.h"
-void populateAddressBook(AddressBook *addressBook);
+#include "contact.h" // Include the contact.h header file in which addressBook structure is defined
+
+void populateAddressBook(AddressBook *addressBook); // Call the populateAddressBook function with addressBook pointer parameter for storing address of addressBook structure
