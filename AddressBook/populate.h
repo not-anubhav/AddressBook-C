401bd5e2f7a8dbdbc899e1485d0149d4d07219a8
Anubhav Anand <notanubhav@gmail.com>
2024-11-29 03:39:47 +0000

    Removed

--- AddressBook/populate.h
+++
@@ -1,3 +0,0 @@
-#include "contact.h" // Include the contact.h header file in which addressBook structure is defined
-
-void populateAddressBook(AddressBook *addressBook); // Call the populateAddressBook function with addressBook pointer parameter for storing address of addressBook structure
